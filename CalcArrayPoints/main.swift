bb5fb5f4446b89fddc6cf46f942f371fb432fe49
emh <emh>
2016-10-10 20:50:50 -0700

    Initial Commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by studentuser on 10/10/16.
+//  Copyright © 2016 emh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
